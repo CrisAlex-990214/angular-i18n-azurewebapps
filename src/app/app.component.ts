1036a0d6a197a02417ee3c0c29f4460ca73775bf
CrisAlex <cads.14alex@outlook.com>
2024-03-01 20:56:58 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Inject, LOCALE_ID, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
@@ -12,11 +12,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class AppComponent {
 
+  constructor(@Inject(LOCALE_ID) public locale: string) { }
+
   products = ["Shirt", "Shoe", "Jacket"];
   filteredProducts = this.products;
   gender = '';
 
   search(value: string) {
     this.filteredProducts = this.products.filter(p => p.includes(value));
+  }
+
+  switchLang() {
+    location.href = this.locale === 'es' ? '/en-US' : '/es';
   }
 }
